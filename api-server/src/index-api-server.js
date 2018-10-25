4efda000150e540c0821d54ff0c03eddbd7167b0
Lorenzo Mussi <lorenzo.mussi@docomodigital.com>
2018-10-25 17:44:00 +0200

    server api origin access enabled for localhost 3000

--- api-server/src/index-api-server.js
+++ api-server/src/index-api-server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import router from './server/routes/index';
+import accessControlMiddleware from './middleware/accessControl.Middleware';
 
 // Set up the express app
 const app = express();
@@ -8,6 +9,9 @@ const app = express();
 // Parse incoming requests data
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+
+// Add  Middleware set headers
+app.use(accessControlMiddleware);
 
 app.use(router);
 
