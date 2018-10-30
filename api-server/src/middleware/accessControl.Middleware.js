64c022bf6b0010cde9d79c9f998b77e9ffe28a70
Lorenzo Mussi <lorenzo.mussi@docomodigital.com>
2018-10-30 14:01:00 +0100

    middleware exposing new localhost development mode

--- api-server/src/middleware/accessControl.Middleware.js
+++ api-server/src/middleware/accessControl.Middleware.js
@@ -3,6 +3,7 @@ export default (req, res, next) => {
 
     // Website you wish to allow to connect
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3333');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
